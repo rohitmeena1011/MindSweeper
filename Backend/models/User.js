142192bcecdda7ab1cdfa797c8b6662769472547
AayushAnand39 <aayushanand737@gmail.com>
2025-03-18 03:17:56 +0530

    Added the global MongoDB database

--- Backend/models/User.js
+++ Backend/models/User.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const UserSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-});
+},{collection : 'Participant Details'});
 
 const User = mongoose.model("User", UserSchema);
 module.exports = User
