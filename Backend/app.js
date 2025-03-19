30c96acbb63b57e80919e6d6e6fb96159b67db99
rohitmeena1011 <rohitmeena3844@gmail.com>
2025-03-19 20:09:03 +0530

    fixed backend issue

--- Backend/app.js
+++ Backend/app.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-  origin: "https://mind-sweeper-api.vercel.app/",
+  origin: "https://mind-sweeper-frontend.vercel.app/",
   methods: ["POST","GET"],
   credentials: true,
 }));
