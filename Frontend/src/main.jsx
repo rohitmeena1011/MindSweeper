b0db4c3db891531c44b27c652713156fb273ec85
AayushAnand39 <aayushanand737@gmail.com>
2025-03-19 14:49:27 +0530

    Tried resolving CSS issues

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -6,8 +6,8 @@ import { AuthProvider } from './AuthContext.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-<AuthProvider>
-    <App />
+    <AuthProvider>
+      <App />
     </AuthProvider>
   </StrictMode>
 );
